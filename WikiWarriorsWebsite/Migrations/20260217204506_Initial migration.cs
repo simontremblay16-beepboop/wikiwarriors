a6115b75ab8e80fa845f142a9be65482d6dac3a2
ckowalski15 <chris01kowalski@gmail.com>
2026-02-17 17:56:07 -0500

    Fixing kennys minor branch issue.

---
+++ WikiWarriorsWebsite/Migrations/20260217204506_Initial migration.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WikiWarriorsWebsite.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initialmigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Fighter",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ImageUrl = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PageUrl = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    WordCount = table.Column<int>(type: "int", nullable: false),
+                    ReferenceCount = table.Column<int>(type: "int", nullable: false),
+                    LinkCount = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Fighter", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Fighter");
+        }
+    }
+}
