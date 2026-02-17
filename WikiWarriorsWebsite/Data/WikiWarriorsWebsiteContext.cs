425e66fb176af0af2560ab62893593d6d82ab744
ckowalski15 <chris01kowalski@gmail.com>
2026-02-17 17:41:23 -0500

    Fingures crossed...?

---
+++ WikiWarriorsWebsite/Data/WikiWarriorsWebsiteContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WikiWarriorsWebsite.Models;
+
+namespace WikiWarriorsWebsite.Data
+{
+    public class WikiWarriorsWebsiteContext : DbContext
+    {
+        public WikiWarriorsWebsiteContext (DbContextOptions<WikiWarriorsWebsiteContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WikiWarriorsWebsite.Models.Fighter> Fighter { get; set; } = default!;
+    }
+}
---
+++ WikiWarriorsWebsite/Models/FightHistory.cs
@@ -0,0 +1,13 @@
+﻿namespace WikiWarriorsWebsite.Models
+{
+    public class FightHistory
+    {
+        public int FightId { get; set; }
+        public List<Fighter> Fighters { get; set; }
+        public int Fighter1Id { get; set; }
+        public int Fighter2Id { get; set; }
+        public int WinnerId { get; set; }
+        public DateTime FightDate { get; set; }
+        public bool DailyFight { get; set; }
+    }
+}
