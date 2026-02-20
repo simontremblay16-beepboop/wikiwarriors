1b2d1ff0b411ef27bd4c7884636a4bc5a4c833be
Simon T <simontremblay16@gmail.com>
2026-02-20 14:24:30 -0500

    Added the Global Fights table (Not fully completed yet) and added some formating for some properties in the FightHistory Data Model.

--- WikiWarriorsWebsite/Models/FightHistory.cs
+++ WikiWarriorsWebsite/Models/FightHistory.cs
@@ -6,18 +6,28 @@ namespace WikiWarriorsWebsite.Models
     public class FightHistory
     {
         public int FightHistoryId { get; set; }
-        public int Fighter1Id { get; set; }
-        public Fighter Fighter1 { get; set; }
+        public int? Fighter1Id { get; set; }
+
+        [Display(Name = "Fighter #1")]
+        public Fighter? Fighter1 { get; set; }
 
         // Note: Fighter2Id, Fighter2, WinnerId, and Winner have to be nullable types.
         // It's a workaround so that SQL is able to delete entries properly.
         // It shouldn't effect anything that we're doing,
         // since we only add entries, not delete them.
         public int? Fighter2Id { get; set; }
+
+        [Display(Name = "Fighter #2")]
         public Fighter? Fighter2 { get; set; }
         public int? WinnerId { get; set; }
+        [Display(Name = "Victor")]
         public Fighter? Winner { get; set; }
+
+        //Should format the date like "2024/06/01 @ 16:45:24"
+        [DisplayFormat(DataFormatString = "{yyyy/MM/dd @ HH:mm:ss}")]
         public DateTime FightDate { get; set; }
+
+        [Display(Name = "Was this a Daily Feature Match?")]
         public bool DailyFight { get; set; }
     }
 }
--- WikiWarriorsWebsite/Pages/Index.cshtml.cs
+++ WikiWarriorsWebsite/Pages/Index.cshtml.cs
@@ -1,13 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using WikiWarriorsWebsite.Data;
+using WikiWarriorsWebsite.Models;
 
 namespace WikiWarriorsWebsite.Pages
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
-        {
+        private readonly WikiWarriorsWebsite.Data.WikiWarriorsWebsiteContext _context;
 
+        public IndexModel(WikiWarriorsWebsite.Data.WikiWarriorsWebsiteContext context)
+        {
+            _context = context;
+        }
+
+        // This is the list of all fights that have happened, ordered by most recent first.
+        public IList<FightHistory> FightHistory { get; set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            FightHistory = await _context.FightHistory.ToListAsync();
         }
     }
 }
