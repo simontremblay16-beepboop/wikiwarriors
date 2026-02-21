a8595541d1e5b236fd420122c450f9d07774d3e3
kya <kchanaul@confederationcollege.ca>
2026-02-21 16:46:30 -0500

    Added files in a separate folder for the fight selection interface

---
+++ AddToProject/SelectionInterface.js
@@ -0,0 +1,38 @@
+function $(id){
+    return document.getElementById(id);
+}
+
+document.addEventListener("DOMContentLoaded", ()=> {
+
+     // https://www.w3schools.com/jsref/dom_obj_event.asp
+    $("").addEventListener("input", wikiSearch) // Search
+    $("").addEventListener("click", selectFighter) // Add fighter
+    $("").addEventListener("click", deselectFighter) //Deselect
+    $("").addEventListener("click", startFight) //Save fighter 
+    $("").addEventListener("click", startFight) // start fight 
+    $("").addEventListener("click", returnHome) // return to homepage
+});
+
+function wikiSearch (){
+    
+}
+
+function selectFighter (){
+
+}
+
+function saveFighter (){
+
+}
+
+function deselectFighter (){
+
+}
+
+function startFight (){
+
+}
+
+function returnHome (){
+
+}
