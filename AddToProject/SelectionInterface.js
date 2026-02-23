f732f7c02ae7f014eddf5722c01265f70cda2aee
kya <kchanaul@confederationcollege.ca>
2026-02-22 20:33:53 -0500

    Added code to the displaySearch function in the SelectionInterface.js

--- AddToProject/SelectionInterface.js
+++ AddToProject/SelectionInterface.js
@@ -6,11 +6,12 @@ document.addEventListener("DOMContentLoaded", ()=> {
 
     // https://www.w3schools.com/jsref/dom_obj_event.asp
     $("search").addEventListener("input", wikiSearch) // Search
-    $("").addEventListener("click", selectFighter) // Add fighter
-    $("").addEventListener("click", deselectFighter) //Deselect
-    $("").addEventListener("click", saveFighter) //Save fighter 
-    $("").addEventListener("click", startFight) // start fight 
-    $("").addEventListener("click", returnHome) // return to homepage
+    $("searchButton").addEventListener("click", wikiSearchButton)
+    $("displaySearch").addEventListener("click", selectFighter) // Add fighter
+    $("displaySearch").addEventListener("click", deselectFighter) //Deselect
+    $("displaySearch").addEventListener("click", saveFighter) //Save fighter 
+    $("displaySearch").addEventListener("click", startFight) // start fight 
+    $("displaySearch").addEventListener("click", returnHome) // return to homepage
 });
     // https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
     //https://developer.mozilla.org/en-US/docs/Glossary/Debounce
@@ -20,7 +21,7 @@ document.addEventListener("DOMContentLoaded", ()=> {
     //prevent the requests to other input events for a period of time 
 let noRequests = false; 
 
-function wikiSearch() {
+async function wikiSearch() {
     // https://www.freecodecamp.org/news/throttling-in-javascript/
     if (noRequests === true) {
         return; 
@@ -43,25 +44,61 @@ function wikiSearch() {
     origin: * //For non-authenticated requests, specify the value *
 */
 
-    let request = `https://www.mediawiki.org/w/api.php?action=query&format=json&origin=*&prop=pageimages%7Credirects%7Cextracts&generator=search&formatversion=2&piprop=thumbnail%7Cname&pithumbsize=50&exchars=150&exlimit=1&exintro=1&explaintext=1&exsectionformat=wiki&gsrsearch=${search}`
+    let request = `https://www.mediawiki.org/w/api.php?action=query&format=json&origin=*&prop=pageimages%7Credirects%7Cextracts&generator=search&formatversion=2&piprop=thumbnail%7Cname&pithumbsize=50&exchars=150&exlimit=1&exintro=1&explaintext=1&exsectionformat=wiki&gsrnamespace=0&gsrsearch=${search}`
+    //! Getting internal documents I think this might be a problem here I will need to fix that. 
 
     // https://www.w3schools.com/js/js_api_fetch.asp
-    let results = fetch(request);
+    // https://www.w3schools.com/js/js_async_await.asp
+    let results = await fetch(request);
+    let wikiSearchResults = await results.json();
 
-
-
-
-
-    //// https://www.w3schools.com/jsref/met_win_settimeout.asp
+    // https://www.w3schools.com/jsref/met_win_settimeout.asp
     setTimeout(() => {
         noRequests = false; 
     }, 2000);
 
-    displaySearch(results);
+    displaySearch(wikiSearchResults);
 }
 
-function displaySearch (results){
-    console.log(results);
+function wikiSearchButton (){
+    noRequests = false;
+    wikiSearch();
+}
+
+
+// remember copy path in console. 
+function displaySearch (wikiSearchResults){
+
+    // if it returns a page then do this if not wait
+    // do not display if no image source is present 
+    // add a button than can allow do the function 
+    // I need to turn each div into a button so be chosen // make it so something when hover over so people know it's clickable 
+
+    let resultsArray = wikiSearchResults.query.pages;
+    console.log(resultsArray);
+
+    let displayCard = 
+
+    //https://www.w3schools.com/jsref/jsref_filter.asp
+    resultsArray.filter(search => search.thumbnail?.source)
+    // https://www.w3schools.com/jsref/jsref_forEach.asp
+    // https://www.w3schools.com/jsref/jsref_map.asp
+
+    // optional chaining ? 
+    .map(
+        search => {
+            return(
+                `<button>
+                    <h5>${search.title}</h5>
+                    <img src="${search.thumbnail?.source}">     
+                    <p>${search.extract}</p>
+                </button>`
+            )
+        }
+        // https://www.w3schools.com/jsref/jsref_join.asp
+    ).join('');
+
+    $("displaySearch").innerHTML = displayCard;
 }
 
 
