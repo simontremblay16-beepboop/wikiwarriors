f1dc54d597fa3b6f8f237bb2eaa045bdd6d8f422
KennyGarstin <kgarstin@confederationcollege.ca>
2026-02-26 13:07:38 -0500

    Created Fight page backend

--- WikiWarriorsWebsite/Pages/Fight/Index.cshtml.cs
+++ WikiWarriorsWebsite/Pages/Fight/Index.cshtml.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -29,21 +30,76 @@ namespace WikiWarriorsWebsite.Pages.Fight
         }
 
         public IList<FightHistory> FightHistory { get;set; } = default!;
+        //public Fighter Fighter1Record { get; set; } = default!;
 
-        public async Task OnGetAsync()
+        [BindProperty]
+        public FightHistory NewFightRecord { get; set; } = default!;
+
+        public SelectList AllFighters { get;set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync()
         {
-            // Fighter names can be accessed through ViewData
-            ViewData["fighter1"] = Fighter1;
-            ViewData["fighter2"] = Fighter2;
+            AllFighters = new SelectList(_context.Fighter, "FighterId", "FighterId");
+            //ViewData["Fighter1Id"] = new SelectList(_context.Fighter, "FighterId", "FighterId");
+            //ViewData["Fighter2Id"] = new SelectList(_context.Fighter, "FighterId", "FighterId");
+            //ViewData["WinnerId"] = new SelectList(_context.Fighter, "FighterId", "FighterId");
+            //return Page();
+            // Fighter ids can be accessed through ViewData
+            int fighter1Id = int.Parse(Fighter1);
+            int fighter2Id = int.Parse(Fighter2);
+            ViewData["fighter1Id"] = fighter1Id;
+            ViewData["fighter2Id"] = fighter2Id;
 
-            // Get fighters images
-            ViewData["fighter1Image"] = "https://upload.wikimedia.org/wikipedia/commons/7/7d/Arch_of_Titus_Menorah.png";
-            ViewData["fighter2Image"] = "https://upload.wikimedia.org/wikipedia/commons/f/fe/Manchester_Central_Library.jpg";
+            // Get database entries for the fighter
+            var fighter1Record = await _context.Fighter.FirstOrDefaultAsync(m => m.FighterId == fighter1Id);
+            var fighter2Record = await _context.Fighter.FirstOrDefaultAsync(m => m.FighterId == fighter2Id);
 
+            if (fighter1Record is not null && fighter2Record is not null)
+            {
+                
+                // Get fighters data
+                ViewData["fighter1Name"] = fighter1Record.Name;
+                ViewData["fighter1ImageUrl"] = fighter1Record.ImageUrl;
+                ViewData["fighter1PageUrl"] = fighter1Record.PageUrl;
+                ViewData["fighter1WordCount"] = fighter1Record.WordCount;
+                ViewData["fighter1ReferenceCount"] = fighter1Record.ReferenceCount;
+                ViewData["fighter1LinkCount"] = fighter1Record.LinkCount;
+
+                ViewData["fighter2Name"] = fighter2Record.Name;
+                ViewData["fighter2ImageUrl"] = fighter2Record.ImageUrl;
+                ViewData["fighter2PageUrl"] = fighter2Record.PageUrl;
+                ViewData["fighter2WordCount"] = fighter2Record.WordCount;
+                ViewData["fighter2ReferenceCount"] = fighter2Record.ReferenceCount;
+                ViewData["fighter2LinkCount"] = fighter2Record.LinkCount;
+
+                return Page();
+            }
+
+            return NotFound();
+
+            /*
             FightHistory = await _context.FightHistory
                 .Include(f => f.Fighter1)
                 .Include(f => f.Fighter2)
-                .Include(f => f.Winner).ToListAsync();
+                .Include(f => f.Winner).ToListAsync();*/
+        }
+
+        // Once the fight has concluded, make a POST request to add the fight record to the database
+        public async Task<IActionResult> OnPostAsync()
+        {
+            //NewFightRecord.Fighter1 = await _context.Fighter.FirstOrDefaultAsync(m => m.FighterId == NewFightRecord.Fighter1Id);
+            //NewFightRecord.Fighter2 = await _context.Fighter.FirstOrDefaultAsync(m => m.FighterId == NewFightRecord.Fighter2Id);
+            //NewFightRecord.Winner = await _context.Fighter.FirstOrDefaultAsync(m => m.FighterId == NewFightRecord.WinnerId);
+            if (!ModelState.IsValid)
+            {
+
+                return Page();
+            }
+
+            _context.FightHistory.Add(NewFightRecord);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("/Index");
         }
     }
 }
