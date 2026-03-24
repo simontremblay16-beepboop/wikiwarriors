8e8b5e2216c91755369bbe4c35654bc61879338b
ckowalski15 <chris01kowalski@gmail.com>
2026-03-24 16:00:04 -0400

    Worked on selection page.

--- WikiWarriorsWebsite/Pages/Selection.cshtml.cs
+++ WikiWarriorsWebsite/Pages/Selection.cshtml.cs
@@ -28,6 +28,7 @@ namespace WikiWarriorsWebsite.Pages
         public async Task<IActionResult> OnPostAsync()
         {
             Results = await _searcher.Search(Name);
+
             return Page();
         }
     }
