c127fdb86548ae57121d26a6c97218efa28a8f59
kya <kchanaul@confederationcollege.ca>
2026-03-28 01:56:34 -0400

    Added selection background placeholder image, added js function to prevent error

--- WikiWarriorsWebsite/Pages/Fight/Index.cshtml.cs
+++ WikiWarriorsWebsite/Pages/Fight/Index.cshtml.cs
@@ -119,13 +119,7 @@ namespace WikiWarriorsWebsite.Pages.Fight
                 int fighter1Score = (Fighter1.LinkCount * Fighter1.ReferenceCount) + Fighter1.WordCount;
                 int fighter2Score = (Fighter2.LinkCount * Fighter2.ReferenceCount) + Fighter2.WordCount;
 
-                // This is unlikely to happen; however...
-                if (fighter1Score == fighter2Score)
-                {
-                    fighter1Score++;
-                }
-
-                if (fighter1Score > fighter2Score)
+                if (fighter1Score >= fighter2Score)
                 {
                     winnerId = Fighter1.FighterId;
                     loserId = Fighter2.FighterId;
